a95ef1be730cd05f0fa7acd56ac675d255d86ef4
Hersh-E <73845631+Hersh-E@users.noreply.github.com>
2020-11-02 14:02:20 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,13 @@
+mkdir -p ~/.streamlit/
 
+echo "\
+[general]\n\
+email = \"hjeason7@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
